c5fe8f11edd6a9ac30df85e36fcc16d78a9c5e62
Shams-Ahnaf-Rafid <83872539+Shams-Ahnaf-Rafid@users.noreply.github.com>
2025-02-06 22:05:29 +0600

    Add files via upload

---
+++ centroid.cpp
@@ -0,0 +1,32 @@
+
+const int N = 1e5+9;
+
+set<int> G[N];
+
+int sz[N], pa[N];
+
+int dfs(int u, int p) {
+  sz[u] = 1;
+  for(auto v : G[u]) if(v != p) {
+    sz[u] += dfs(v, u);
+  }
+  return sz[u];
+}
+int centroid(int u, int p, int n) {
+  for(auto v : G[u]) if(v != p) {
+    if(sz[v] > n / 2) return centroid(v, u, n);
+  }
+  return u;
+}
+void build(int u, int p) {
+  int n = dfs(u, p);
+  int c = centroid(u, p, n);
+  if(p == -1) p = c;
+  pa[c] = p;
+
+  vector<int> tmp(G[c].begin(), G[c].end());
+  for(auto v : tmp) {
+    G[c].erase(v); G[v].erase(c);
+    build(v, c);
+  }
+}
